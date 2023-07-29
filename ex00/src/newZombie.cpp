23502e2059afdc082961355a612a9f1d7f87328a
Bruno Azuara <bruno.azuara@gmail.com>
2023-07-29 19:45:19 +0200

    relocate functions to speific files

--- ex00/src/newZombie.cpp
+++ ex00/src/newZombie.cpp
@@ -1,25 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bazuara <bazuara@student.42madrid.co       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/29 19:35:05 by bazuara           #+#    #+#             */
-/*   Updated: 2023/07/29 19:44:52 by brunoazu         ###   ########.fr       */
+/*   Created: 2023/07/29 19:38:03 by bazuara           #+#    #+#             */
+/*   Updated: 2023/07/29 19:38:08 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <zombie.h>
 
-int main() {
-  // Crear un zombie en el heap
-  Zombie* zombie1 = newZombie("Alice");
-  zombie1->announce();
-  delete zombie1;
-
-  // Crear un zombie en el stack
-  randomChump("Bob");
-
-  return 0;
+Zombie* newZombie(std::string name) {
+    return new Zombie(name);
 }
--- ex00/src/randomChump.cpp
+++ ex00/src/randomChump.cpp
@@ -1,25 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bazuara <bazuara@student.42madrid.co       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/29 19:35:05 by bazuara           #+#    #+#             */
-/*   Updated: 2023/07/29 19:44:52 by brunoazu         ###   ########.fr       */
+/*   Created: 2023/07/29 19:39:02 by bazuara           #+#    #+#             */
+/*   Updated: 2023/07/29 19:39:08 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <zombie.h>
 
-int main() {
-  // Crear un zombie en el heap
-  Zombie* zombie1 = newZombie("Alice");
-  zombie1->announce();
-  delete zombie1;
-
-  // Crear un zombie en el stack
-  randomChump("Bob");
-
-  return 0;
+void randomChump(std::string name) {
+    Zombie zombie(name);
+    zombie.announce();
 }
+
