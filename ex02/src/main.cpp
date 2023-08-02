d2a560e780b9546629d3dfcfa01b51736e40f6c4
Bruno Azuara <bruno.azuara@gmail.com>
2023-08-02 12:25:52 +0200

    add ex02

---
+++ ex02/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazuara <bazuara@student.42madrid.co       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 12:12:59 by bazuara           #+#    #+#             */
+/*   Updated: 2023/08/02 12:23:04 by bazuara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main(void) {
+    std::string     str = "HI THIS IS BRAIN";
+    std::string     *stringPTR = &str;
+    std::string     &stringREF = str;
+
+    std::cout << "string    : " << str << std::endl;
+    std::cout << "stringPTR : " << *stringPTR << std::endl;
+    std::cout << "stringREF : " << stringREF << std::endl;
+    return (0);
+}
